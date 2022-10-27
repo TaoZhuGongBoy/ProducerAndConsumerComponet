3a715d3d64b0fe9254e82d33af2e913eb7212411
zhangfengshi <zhangfengshi@startdt.com>
2022-10-27 20:47:14 +0800

    1

---
+++ src/main/java/com/taozhugongboy/concurrentprogramming/ConcurrentProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.taozhugongboy.concurrentprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrentProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrentProgrammingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taozhugongboy/concurrentprogramming/ConcurrentProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taozhugongboy.concurrentprogramming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrentProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
