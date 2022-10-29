cf45bb3602c68954f7a9e3825d12312a3f8e24f5
taozhugongBoy <zhangfengshi@startdt.com>
2022-10-29 21:31:34 +0800

    添加测试用例

--- src/main/java/com/taozhugongboy/concurrentprogramming/componet/ProducerAndConsumerComponet.java
+++ src/main/java/com/taozhugongboy/concurrentprogramming/componet/ProducerAndConsumerComponet.java
@@ -1,6 +1,8 @@
 package com.taozhugongboy.concurrentprogramming.componet;
 
 import com.taozhugongboy.concurrentprogramming.Processor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,18 +16,19 @@ import java.util.concurrent.locks.LockSupport;
  * @author taozhugongBoy
  */
 public class ProducerAndConsumerComponet<T> {
+    private final static Logger log = LoggerFactory.getLogger(ProducerAndConsumerComponet.class);
 
     //组件持有一个工作线程对象数组
     private final WorkThread<T>[] workThreads;
     private AtomicInteger index;
     private static final Random r = new Random();
     //任务定时器
-    private static ScheduledExecutorService TIMER = new ScheduledThreadPoolExecutor(1);
-
-    private static ExecutorService POOL = Executors.newCachedThreadPool();
+    private static ScheduledExecutorService scheduleThreadPool = new ScheduledThreadPoolExecutor(1);
+    //组件初始化完成工作线程的新建
+    private static ExecutorService executorService = Executors.newCachedThreadPool();
 
     /**
-     * 组件构造器
+     * 构造器
      * @param threadNum 默认新建的消费者线程个数
      * @param limitSize 队列长度阈值;超过将唤醒阻塞的线程
      * @param period 前后两个任务的执行周期 (for example :200ms 代表前面一次任务执行完毕后，200毫秒后下一个任务继续执行)
@@ -41,23 +44,28 @@ public class ProducerAndConsumerComponet<T> {
             WorkThread<T> workThread = new WorkThread("workThread"+ "_" + i, limitSize, period, capacity, processor);
             this.workThreads[i] = workThread;
 
-            POOL.submit(workThread);
-            TIMER.scheduleAtFixedRate(workThread::timeout, r.nextInt(50), period, TimeUnit.MILLISECONDS);
+            executorService.submit(workThread);
+            //调用scheduleAtFixedRate时，会向ScheduledThreadPoolExecutor的DelayQueue添加一个实现了RunableScheduleFuture接口的
+            //ScheduleFutureTask
+            scheduleThreadPool.scheduleAtFixedRate(workThread::timeout, r.nextInt(50), period, TimeUnit.MILLISECONDS);
         }
     }
 
     /**
-     * 生产者将对象添加到对应消费者线程对象的阻塞队列中去<br>
-     * 采用HASH取模算法进行动态路由
+     * 生产者线程将对象添加到对应消费者线程对象内部的阻塞队列中去<br>
+     * 内部采用HASH取模算法进行动态路由
      * @param item 待添加的对象
      * @return true:添加成功 false:添加失败
      */
     public boolean add(T item) {
+       // log.info("add item={}",item);
         int len = this.workThreads.length;
+        //log.info("add len..."+len);
         if (len == 1) {
             return this.workThreads[0].add(item);
         } else {
             int mod = this.index.incrementAndGet() % len;
+         //   log.info("路由到this.workThreads[mod]={}",mod);
             return this.workThreads[mod].add(item);
         }
     }
@@ -67,24 +75,25 @@ public class ProducerAndConsumerComponet<T> {
      */
     private static class WorkThread<T> implements Runnable {
         /**
-         * 工作线程名字
+         * 工作线程命名
          */
         private final String threadName;
         /**
-         * 队列中指定元素个数限制
+         * 队列中允许存放元素个数限制<br>
+         * 超出将从队列中取出此大小的元素转成List对象
          */
         private final int queueSizeLimit;
         /**
-         * 距离上次任务处理时间的间隔限制
+         * 前后两个任务的执行周期
          */
-        private int flushInterval;
+        private int period;
         /**
-         * 用来记录任务处理的最新时间
+         * 用来记录任务的即时处理时间
          */
         private volatile long lastFlushTime;
 
         /**
-         * 当前线程对象
+         * 当前工作线程对象
          */
         private volatile Thread currentThread;
 
@@ -93,22 +102,23 @@ public class ProducerAndConsumerComponet<T> {
          */
         private final BlockingQueue<T> queue;
         /**
-         * 回调函数
+         * 回调接口
          */
         private final Processor<T> processor;
+
 
         /**
          * 消费者线程构造器
          * @param threadName 线程名
-         * @param queueSizeLimit 指定队列阈值
-         * @param flushInterval 指定距离时间阈值
-         * @param capacity 阻塞队列初始化大小
+         * @param queueSizeLimit 指定队列阈值(可配置)
+         * @param period 前后两个任务的执行周期(可配置)
+         * @param capacity 阻塞队列初始容量
          * @param processor 回调接口
          */
-        public WorkThread(String threadName, int queueSizeLimit, int flushInterval, int capacity, Processor<T> processor) {
+        public WorkThread(String threadName, int queueSizeLimit, int period, int capacity, Processor<T> processor) {
             this.threadName = threadName;
             this.queueSizeLimit = queueSizeLimit;
-            this.flushInterval = flushInterval;
+            this.period = period;
             this.lastFlushTime = System.currentTimeMillis();
             this.processor = processor;
             this.queue = new ArrayBlockingQueue(capacity);
@@ -120,16 +130,20 @@ public class ProducerAndConsumerComponet<T> {
          * @return true:添加成功 false:添加失败
          */
         public boolean add(T item) {
+        //    log.info("add result:"+item);
             boolean result = this.queue.offer(item);
+          //  log.info("resultP{}",result);
             this.checkQueueSize();
             return result;
         }
 
         /**
-         * 当前时间与上次的任务处理时间差是否超过指定阈值;如果超过触发start方法
+         * 当前时间与上次任务处理时间差是否超过指定阈值;如果超过触发start方法
          */
         public void timeout() {
-            if (System.currentTimeMillis() - this.lastFlushTime >= (long)this.flushInterval) {
+          //  log.info("{}====check timeout",currentThread.getName());
+            if (System.currentTimeMillis() - this.lastFlushTime >= (long)this.period) {
+                log.info("当前时间距离上次任务处理时间周期={}超出指定阈值={}",System.currentTimeMillis() - this.lastFlushTime ,period);
                 this.start();
             }
 
@@ -139,6 +153,7 @@ public class ProducerAndConsumerComponet<T> {
          * 唤醒被阻塞的工作线程
          */
         private void start() {
+            log.info("执行start方法，唤醒被阻塞的线程"+currentThread.getName());
             LockSupport.unpark(this.currentThread);
         }
 
@@ -147,6 +162,7 @@ public class ProducerAndConsumerComponet<T> {
          */
         private void checkQueueSize() {
             if (this.queue.size() > this.queueSizeLimit) {
+                log.info("{}队列大小={}超出指定阈值={}",currentThread.getName(),this.queue.size() ,queueSizeLimit);
                 this.start();
             }
 
@@ -159,10 +175,12 @@ public class ProducerAndConsumerComponet<T> {
             if(queue.isEmpty()){
                 return;
             }
+
             this.lastFlushTime = System.currentTimeMillis();
             List<T> temp = new ArrayList(this.queueSizeLimit);
             int size = this.queue.drainTo(temp, this.queueSizeLimit);
             if (size > 0) {
+                log.info("{}被唤醒后,开始执行任务:从队列中腾出大小为{}的数据且转成List对象",currentThread.getName(),size);
                 try {
                     //执行回调函数
                     this.processor.process(temp);
@@ -178,7 +196,7 @@ public class ProducerAndConsumerComponet<T> {
          * @return true:满足触发条件 false:不满足触发条件
          */
         private boolean canFlush() {
-            return this.queue.size() > this.queueSizeLimit || System.currentTimeMillis() - this.lastFlushTime > (long)this.flushInterval;
+            return this.queue.size() > this.queueSizeLimit || System.currentTimeMillis() - this.lastFlushTime > (long)this.period;
         }
 
         @Override
@@ -187,13 +205,15 @@ public class ProducerAndConsumerComponet<T> {
             this.currentThread.setName(this.threadName);
             //当前线程没有被其他线程打断
             while(!this.currentThread.isInterrupted()) {
-                //死循环的判断是否满足触发条件(队列实际大小是否超出指定阈值或距离上次任务时间是否超出指定阈值)，如果未满足将阻塞当前线程，避免死循环给系统带来性能影响
+                //死循环的判断是否满足触发条件(队列实际大小是否超出指定阈值或距离上次任务时间是否超出指定阈值)，如果未满足将阻塞当前线程，避免死循环给系统带来性能开销
                 while(!this.canFlush()) {
-                    //当前线程被阻塞
+                    //当前工作线程被阻塞
+                    log.info("线程被阻塞...");
                     LockSupport.park(this);
                 }
-                //一旦add执行的时候判断实际工作线程对象内部的阻塞队列大小超出自定制阈值亦或距离上次任务处理时间差超出指定阈值，就会调用LockSupport.unpark方法解除阻塞的线程
+                //一旦add方法执行的时候判断存放的阻塞队列元素大小超出自定制阈值亦或距离上次任务处理时间差超出指定阈值，就会调用LockSupport.unpark方法解除阻塞的线程
                 //一旦线程被解除阻塞，就会触发此方法，将队列元素转成List对象且调用已经注册的回调函数
+              //  log.info("阻塞线程被唤醒");
                 this.flush();
             }
 
--- src/test/java/com/taozhugongboy/concurrentprogramming/ConcurrentProgrammingApplicationTests.java
+++ src/test/java/com/taozhugongboy/concurrentprogramming/ConcurrentProgrammingApplicationTests.java
@@ -1,11 +1,14 @@
 package com.taozhugongboy.concurrentprogramming;
 
 import com.taozhugongboy.concurrentprogramming.componet.ProducerAndConsumerComponet;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.TimeUnit;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = ConcurrentProgrammingApplication.class)
@@ -15,11 +18,25 @@ class ConcurrentProgrammingApplicationTests {
 	private ProducerAndConsumerComponet producerAndConsumerComponet;
 
 
+	/**
+	 * 前置条件：
+	 *     #主件初始化默认新增两个工作线程
+	 *     config.threadNum=2
+	 *     config.period=12000
+	 *     config.queueSizeLimit=3
+	 *     config.capacity=10
+	 */
+	@DisplayName("用例一:队列大小超出指定阈值")
 	@Test
 	void add() {
 		for(int i=0;i<10;i++){
 
 			producerAndConsumerComponet.add("1");
+		}
+		try {
+			TimeUnit.SECONDS.sleep(10);
+		}catch (Exception e){
+			e.printStackTrace();
 		}
 	}
 
